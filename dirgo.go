97dec56fa7281c0c79f56b02565a1e4aca9eb6c6
edi <BBerastegui@users.noreply.github.com>
2015-04-20 16:42:49 +0200

    Moar. WIP.

--- dirgo.go
+++ dirgo.go
@@ -5,21 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"strconv"
 	"sync"
-	"time"
 )
 
-func random(min, max int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(max-min) + min
-}
+var red = "\\033[0;31m"
 
 // Channel of simultaneous tasks
 var simul = make(chan string, 10)
 var task_queue = make(chan string)
+var wg sync.WaitGroup
 
 // Array of strings with finished tasks
 var finished_tasks []string
@@ -28,36 +24,42 @@ var finished_tasks []string
 var found_dir []string
 var found_files []string
 
-// Array with pending directories
-var pending_dir []string
-
 // TODO
 // Persistence variables
 // Last word used
 var lastword string
 
-var wg sync.WaitGroup
+// Array with pending directories
+var pending_dir []string
 
-var url string
+var targetUrl string
 var dict string
 
 func main() {
-	// TODO
-	// Fix flexible URL input. Ex.: http://XXX.com OR XXX.com
 	// FLAGS
 	urlFlag := flag.String("u", "foo", "the url to test")
 	dictFlag := flag.String("d", "foo", "the dictionary to use")
 	flag.Parse()
 
-	url = *urlFlag
+	targetUrl = formatUrl(*urlFlag)
 	dict = *dictFlag
 	// /FLAGS
 
-	// ---
+	// BANNER
+	fmt.Println("[/!\\] Starting bruteforcing with dict: \n\t" + dict + "\n   On site: " + targetUrl + "\n")
+	// /BANNER
+
+	// Run !
 	go feed(task_queue, "")
 	consume(task_queue, simul)
 	wg.Wait()
-	fmt.Println(len(finished_tasks), " finished tasks.")
+
+	// Finished.
+	fmt.Println("\n[FINISHED]\n\n"+len(finished_tasks), " finished tasks.")
+	fmt.Println("\n[i] Directories found: ")
+	fmt.Println(found_dir)
+	fmt.Println("[i] Files found: ")
+	fmt.Println(found_files)
 }
 
 // This function will feed the task channel
@@ -80,24 +82,24 @@ func feed(task_queue chan string, prefix string) {
 
 	// If it has no prefix (first time called)
 	if len(prefix) == 0 {
-		fmt.Println("\r--- [i] Waiting for tasks to end and close the task_queue.")
+		fmt.Println("\r--- [i] Waiting for tasks to end and close the task_queue.\n")
 		wg.Wait()
 		close(task_queue)
 	}
-	fmt.Println("\r--- [i] Task feed finished for prefix: " + prefix)
+	fmt.Println("\r--- [i] Task feed finished for prefix: " + prefix + "\n")
 }
 
 func consume(task_queue chan string, simul chan string) {
 	for path := range task_queue {
 		simul <- path
-		go scan(url, path, simul)
+		go scan(targetUrl, path, simul)
 	}
 	// All tasks consumed
 }
 
-func scan(url string, path string, simul chan string) {
+func scan(targetUrl string, path string, simul chan string) {
 	wg.Add(1)
-	response, content, _ := httpRequest(url, path, false)
+	response, content, _ := httpRequest(targetUrl, path, false)
 	/*	if err != nil {
 			log.Print("[Request error] %s", err)
 		}
@@ -108,35 +110,44 @@ func scan(url string, path string, simul chan string) {
 		fmt.Printf("\r %s", path)
 	case response.StatusCode == 200:
 		if isListable(content) {
-			fmt.Println("\r[!] Path: " + path + " is listable.")
+			fmt.Println("\r\033[32m\033[1m[!]\033[0m Path: " + path + " is listable.")
+			// Add directory to the found list
+			found_dir = append(found_dir, path+" (LISTABLE)")
 		} else {
+
 			fmt.Printf("\r                                ")
-			fmt.Println("\r[!] 200 on: " + path + " - Size: " + strconv.Itoa(len(content)))
+			fmt.Println("\r\033[32m\033[1m[!]\033[0m 200 on: " + path + " - Size: " + strconv.Itoa(len(content)))
+			found_files = append(found_files, path+" - Size: "+strconv.Itoa(len(content)))
 		}
 	case response.StatusCode >= 300 && response.StatusCode <= 399:
 		// TODO
 		// Fix print race-condition problems
 		fmt.Printf("\r                                ")
-		fmt.Println("\r[!] Response.Statuscode == " + strconv.Itoa(response.StatusCode) + " on " + path + " - Size: " + strconv.Itoa(len(content)))
+		fmt.Println("\r\033[32m\033[1m[!]\033[0m Response.Statuscode == " + strconv.Itoa(response.StatusCode) + " on " + path + " - Size: " + strconv.Itoa(len(content)))
 		if isDirectory(response, path) {
-			fmt.Println("    [i] " + path + " is a directory.")
+			fmt.Println("\r   [i] " + path + " is a directory.")
 			// Call to httpRequest with following redirects
-			response, content, _ := httpRequest(url, path, true)
+			response, content, _ := httpRequest(targetUrl, path, true)
 			if isListable(content) {
 				fmt.Println("\r    └[i] Directory: " + path + " is listable. It won't be added to queue.")
+				found_dir = append(found_dir, path+"(LISTABLE)")
 			} else {
 				fmt.Println("\r    └[i] Directory: " + path + " ended with a " + strconv.Itoa(response.StatusCode) + " It will be added to queue.")
 				feed(task_queue, path+"/")
+				found_dir = append(found_dir, path)
 			}
 		} else {
-			fmt.Println("    [i] " + path + " is NOT a directory.")
+			fmt.Println("\r    [i] " + path + " is NOT a directory.")
+			found_files = append(found_files, path+" - Size: "+strconv.Itoa(len(content)))
 		}
 	case response.StatusCode == 403:
 		fmt.Printf("\r                                ")
-		fmt.Println("\r[!] 403 on " + path + " - Size: " + strconv.Itoa(len(content)))
+		fmt.Println("\r\033[33m\033[1m[!]\033[0m 403 on " + path + " - Size: " + strconv.Itoa(len(content)))
+		found_files = append(found_files, path+" - Size: "+strconv.Itoa(len(content)))
 	case response.StatusCode == 405:
 		fmt.Printf("\r                                ")
-		fmt.Println("\r[!] 405 on " + path + " - Size: " + strconv.Itoa(len(content)))
+		fmt.Println("\r\033[34m\033[1m[!]\033[0m 405 on " + path + " - Size: " + strconv.Itoa(len(content)))
+		found_files = append(found_files, path+" - Size: "+strconv.Itoa(len(content)))
 	}
 
 	finished_tasks = append(finished_tasks, <-simul)
--- dirgo_funcs.go
+++ dirgo_funcs.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"regexp"
 )
 
@@ -28,48 +29,47 @@ func isDirectory(response *http.Response, path string) bool {
 	}
 }
 
-func httpRequest(url string, path string, followRedirect bool) (response *http.Response, content []byte, err error) {
+func formatUrl(urlToParse string) (formattedUrl string) {
+	u, err := url.Parse(urlToParse)
+	if err != nil {
+		panic(err)
+	}
+	if len(u.Scheme) == 0 {
+		u.Scheme = "http"
+		return u.String() + "/"
+	} else {
+		if len(u.Path) == 0 {
+			u.Path = "/"
+			return u.String()
+		} else {
+			return u.String()
+		}
+	}
+}
+
+func httpRequest(targetUrl string, path string, followRedirect bool) (response *http.Response, content []byte, err error) {
 	client := &http.Client{}
 	// If its requested not to follow redirects
 	if !followRedirect {
 		client = &http.Client{
 			CheckRedirect: func(req *http.Request, via []*http.Request) error {
-				return errors.New("Don't redirect!")
+				return errors.New("NotDon't redirect!")
 			},
 		}
 	} else {
 		client = &http.Client{}
 	}
 	// ---
+
 	// Perform HTTP request
-	req, err := http.NewRequest("GET", url+path, nil)
+	req, err := http.NewRequest("GET", targetUrl+path, nil)
 	response, err = client.Do(req)
 
-	// TODO
-	/*if err != nil {
-		if response.StatusCode == 301 {
-			fmt.Println("\n[i] Got 301 on " + path)
-			content = []byte("")
-			return response, content, err
-		} else {
-			// Not a 301, re-perform request with follow redirects.
-			//log.Printf("HTTP request failed.")
-			client = &http.Client{}
-			req, err := http.NewRequest("GET", url+path, nil)
-			if err != nil {
-				fmt.Println("\n Error performing request:")
-			}
-			response, err = client.Do(req)
-			if err != nil {
-				fmt.Println("\n Error doing request:")
-			}
-		}
-	}
-	*/
 	defer response.Body.Close()
 	content, err = ioutil.ReadAll(response.Body)
 	if err != nil {
 		// Return. Error on reading content.
+		// We don't really care.
 		return response, content, err
 	}
 	// Return. Everything went OK
